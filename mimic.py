deca0abe0ae93c38162ac010997c9c9e1b3e76ba
Michelle Hoelzer <hoelzermm@gmail.com>
2019-02-05 11:56:08 -0500

    finished both functions properly?; added test text file

--- mimic.py
+++ mimic.py
@@ -29,6 +29,7 @@ With the mimic dict, it's fairly easy to emit random
 text that mimics the original. Print a word, then look
 up what words might come next and pick one at random as
 the next work.
+Key is the word, and values are w/e follows 
 Use the empty string as the first word to prime things.
 If we ever get stuck with a word that is not in the dict,
 go back to the empty string to keep things moving.
@@ -48,21 +49,38 @@ import sys
 
 
 def mimic_dict(filename):
-    """Returns mimic dict mapping each word to list of words which follow it."""
-    # +++your code here+++
-    raise NotImplementedError("Get to Work!")
+    """Returns mimic dict mapping each word to list of words that follow it."""
+    words_dict = {}
+    with open(filename, "r") as read_opened_file:
+        words = read_opened_file.read().split()
+    initial_word = ""
+    for word in words:
+        if initial_word in words_dict:
+            words_dict[initial_word].append(word)
+            # KV pair; adds on another instance that goes along with the key
+        else:
+            words_dict[initial_word] = [word]
+            # KV pair; adding to words_dict
+            # no appending strings to list, so have to make it a list
+        initial_word = word
+    # print(words_dict)
+    return words_dict
 
 
 def print_mimic(mimic_dict, word):
     """Given mimic dict and start word, prints 200 random words."""
-    # +++your code here+++
-    raise NotImplementedError("Get to Work!")
+    word_count = 0
+    while word_count <= 200:
+        chosen_word = mimic_dict.get(word)
+        word = random.choice(chosen_word)
+        word_count += 1
+        print word,
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
     if len(sys.argv) != 2:
-        print 'usage: python mimic.py file-to-read'
+        print('usage: python mimic.py file-to-read')
         sys.exit(1)
 
     d = mimic_dict(sys.argv[1])
